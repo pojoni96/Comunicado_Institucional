3c0673782d507952397190f6e3fb01ae8319b72d
JAMontana <65740883+pojoni96@users.noreply.github.com>
2023-11-14 17:21:23 -0300

    Borrado de linea de console

---
+++ JS/actualizarComunicado.js
@@ -0,0 +1,95 @@
+/**
+ * Crea un elemento "a", que tomara como vinculo al elemento pasado por parametro.
+ * @param {HTMLDivElement} div 
+ */
+function irAlElemento(div){
+    let a = document.createElement("a");
+    a.setAttribute("href", div);
+    a.click();
+    a.remove();
+}
+
+/**
+ * En el caso de que la opcion sea "Copiar" setea el margen superior del contenedor de botones en 0.
+ * @param {String} opcion 
+ */
+function quitarMargenSuperior(opcion){
+    let div = document.getElementById("contenedor_botones_modificar-gestionar_comunicado");
+
+    if(opcion == "Copiar")
+        div.style.marginTop = 0;
+    else
+        div.style.marginTop = "20px";
+}
+
+/**
+ * Devuelve la opcion Descargar o Copiar del formulario.
+ * @returns {HTMLFormElement}
+ */
+function conseguirOpcionComunicado(){
+    let valor = document.querySelector("input[name='opcionComunicado']:checked");
+    let st;
+
+    if(valor == null)
+        st = false;
+    else
+        st = valor.value;
+    
+    return st;
+}
+
+/**
+ * Valida las respuesta del formulario, e inserta los textos en el comunicado que corresponda.
+ * Retorna si se actualizo el comunicado o no.
+ * @return {Boolean}
+ */
+function actualizarComunicado(){
+    let campos_validos;
+    let campo_fecha = document.getElementById("campo_fecha").value;
+    let campo_texto_comunicado = String(document.getElementById("campo_texto_comunicado").value);
+    let opcion_comunicado = conseguirOpcionComunicado();
+
+    if(campo_fecha.length !=0 && campo_texto_comunicado !=0 && opcion_comunicado){
+        insertarComunicado(opcion_comunicado);
+        actualizarNombreBoton(opcion_comunicado);
+        quitarMargenSuperior(opcion_comunicado);
+        
+        document.getElementById("campo_fecha_a_modificar").innerHTML = campo_fecha;
+        let texto = campo_texto_comunicado.replace(/\n|\r/g, "<br>");
+        document.getElementById("campo_texto_a_modificar").innerHTML = texto;
+        
+        campos_validos = true;
+    }
+    else
+        campos_validos = false;
+
+    return campos_validos;
+}
+
+/**
+ * Agrega una clase al body que cambia el color de fondo.
+ */
+function cambiarColoresAlBody(){
+    document.querySelector("body").classList.add("nuevos_colores_del_body");
+}
+
+/**
+ * Muestra el bloque que contiene el comunicado y los botones.
+ */
+function mostrarContenidoOculto(){
+    let div = document.getElementById("bloque_contenido_oculto");
+
+    div.classList.remove("bloque_contenido_oculto");
+    div.classList.add("mostrar_bloque_oculto");
+}
+
+/**
+ * Actualiza la pagina, utilizando las funciones anteriores.
+ */
+function actualizarPagina(){
+    if(actualizarComunicado()){
+        cambiarColoresAlBody();
+        mostrarContenidoOculto();
+        irAlElemento("#cuerpo_del_comunicado");
+    }
+}
---
+++ JS/copiarYDescargarComunicado.js
@@ -0,0 +1,38 @@
+/**
+ * Copia el comunicado en el portapapales.
+ */
+function copiarComunicado(){
+    let div = document.getElementById("seccion_del_comunicado").innerHTML;
+
+    navigator.clipboard.writeText(div);
+}
+
+/**
+ * Descarga el comunicado en formato PNG.
+ */
+function descargarComunicado(){
+    const comunicado = document.getElementById("cuerpo_del_comunicado");
+    let nombre_archivo = "CI-" + String(document.getElementById("campo_fecha").value).replace(/\s/g,'_') + ".png";
+
+    html2canvas(comunicado, {scale: 5, allowTaint: true, useCORS: true}).then((canvas) => {
+        let base64img= canvas.toDataURL("image/png");
+        base64img.crossOrigin = "anonymous";
+        let aux_hlink = document.createElement("a");
+        aux_hlink.setAttribute("href", base64img);
+        aux_hlink.setAttribute("download", nombre_archivo);
+        aux_hlink.click();
+        aux_hlink.remove();
+    });
+}
+
+/**
+ * Dependiendo de la opcion del comunicado, se descarga la imagen o copia el codigo HTML.
+ */
+function gestionarComunicado(){
+    let opcion = conseguirOpcionComunicado();
+
+    if(opcion == "Descargar")
+        descargarComunicado();
+    else
+        copiarComunicado();
+}
